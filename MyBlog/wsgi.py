39034a1b6ce31e7d93397d25178b216f5a2d1021
jeremy <865889915@qq.com>
2020-01-20 11:48:03 +0800

    修改配置文件

--- MyBlog/wsgi.py
+++ MyBlog/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyBlog.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyBlog.settings.prod')
 
 application = get_wsgi_application()
